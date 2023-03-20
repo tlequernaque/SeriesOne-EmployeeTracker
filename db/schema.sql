278cc3e50324b4219bd94f1c6503c37f9b009baf
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-20 01:51:03 -0400

    added to schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT
@@ -21,7 +21,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
